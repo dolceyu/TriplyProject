3b428eb2913d8407a3df36214756ea75b220b7c2
dolceyu <dolceyu>
2026-05-21 02:11:49 +0300

    DB fix

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:QAdp3kYU28@localhost:5432/triply_db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://postgres:QAdp3kYU28@localhost:5432/triply_db"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
